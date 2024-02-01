c0c9d5bea58731a037095b970a0e7cf2e4a60e9d
sandeepkumar988 <155413817+sandeepkumar988@users.noreply.github.com>
2024-02-01 10:01:20 +0530

    Change

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 // For navbar responsive (toggle button action)
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let body = document.querySelector('body');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open')
+    navbar.classList.toggle('open');
+    // body.classList.toggle("no-scroll");
 }
